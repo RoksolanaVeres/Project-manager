114071b6989293a66c7f3c15c88577e0ceb63e49
Roksolana Veres <Roxolanka1@ukr.net>
2024-04-26 23:29:12 +0300

    feat: due date

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import { CalendarIcon } from "@radix-ui/react-icons";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+
 export default function ProjectForm() {
+  const [date, setDate] = useState(null);
+
   return (
     <div className="grid gap-8">
       <div id="buttons-container" className="flex gap-2 justify-end">
@@ -12,14 +22,14 @@ export default function ProjectForm() {
       </div>
 
       <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="name" className="text-base">
-          Project Name:
+        <Label htmlFor="name" className="text-base uppercase font-bold">
+          Title
         </Label>
-        <Input type="text" id="name" placeholder="Project name" className="text-base" />
+        <Input type="text" id="name" placeholder="Project title" className="text-base" />
       </div>
 
       <div className="grid w-full gap-1.5 ">
-        <Label htmlFor="description" className="text-base">
+        <Label htmlFor="description" className="text-base uppercase font-bold">
           Description:
         </Label>
         <Textarea
@@ -27,6 +37,30 @@ export default function ProjectForm() {
           id="description"
           className="text-base"
         />
+      </div>
+
+      <div className="grid w-full gap-1.5 ">
+        <Label htmlFor="dueDate" className="text-base uppercase font-bold">
+          Due Date:
+        </Label>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              id="dueDate"
+              variant={"outline"}
+              className={cn(
+                "justify-start text-left font-normal text-base",
+                !date && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {date ? format(date, "PPP") : <span>Pick a date</span>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ export default function Sidebar() {
   return (
     <aside className=" bg-secondary text-foreground w-[25%] mt-8 rounded-tr-xl px-10 pt-20">
       <h1 className="uppercase text-primary font-bold text-xl mb-6">Your Projects</h1>
-      <Button>+ Add project</Button>
+      <Button className="text-base">+ Add project</Button>
     </aside>
   );
 }
